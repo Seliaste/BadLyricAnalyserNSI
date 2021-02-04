5084634c47e9ee8e40c0cf3a55a26451d3b1c37d
Seliaste <aena.aria@protonmail.com>
2021-02-04 10:48:59 +0100

    Added scrapping based on genius

--- scraplyrics.py
+++ scraplyrics.py
@@ -1,15 +1,22 @@
 import bs4 as bs
 import urllib.request
 
+def scrap(site):
+    siteSplitted = site.split("/")
+    lyricsName=str(siteSplitted[len(siteSplitted)-1])
+    vraiSite= "https://genius.com/amp/"+lyricsName
+    print(lyricsName)
+    print(vraiSite)
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib.request.Request(vraiSite,headers=hdr)
+    saucel=urllib.request.urlopen(req)
+    soupl = bs.BeautifulSoup(saucel, 'html')
 
-hdr = {'User-Agent': 'Mozilla/5.0'}
-site= "https://genius.com/amp/Damso-feu-de-bois-lyrics"
-req = urllib.request.Request(site,headers=hdr)
-saucel=urllib.request.urlopen(req)
-soupl = bs.BeautifulSoup(saucel, 'html')
 
+    tmp = soupl.find('div', "lyrics")
+    lyrics = open('temp/lyrics.txt','w')
+    lyrics.write(tmp.text)
+    lyrics.close()
 
-tmp = soupl.find('div', "lyrics")
-lyrics = open('lyrics.txt','w')
-lyrics.write(tmp.text)
-lyrics.close()
+if __name__ == '__main__':
+    scrap("https://genius.com/Alpha-wann-soldat-tue-soldat-lyrics")
