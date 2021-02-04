7967e09c4cce573aa46ed2fc8db8e02e5936768e
Seliaste <aena.aria@protonmail.com>
2021-02-04 11:20:17 +0100

    changed some things I don't remember what

---
+++ main.py
@@ -0,0 +1,21 @@
+import scraplyrics
+import json
+
+def openData(jsonFilePath):
+    file = open(str(jsonFilePath),"r") 
+    data = json.load(file)
+    return data
+
+def main():
+    url=input("Quel est le lien Genius: ")
+    text=scraplyrics.scrap(url)
+    lexicalFieldData=openData("champLexicaux.json")
+    print(lexicalFieldData)
+    
+
+
+
+
+
+if __name__=='__main__':
+    main()
