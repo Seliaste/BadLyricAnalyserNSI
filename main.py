f043f4b32f46e40c9b814991323e6627344027e5
Seliaste <aena.aria@protonmail.com>
2021-02-11 10:57:31 +0100

    Improvements

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-import scraplyrics    #on importe la bibliothèque scraplyrics
-import json      #on importe le fichier json
-import boyermoore      #on importe le fichier boyer_moore
-import matplotlib.pyplot as plt      #on importe la bibliothèque matplotlib
+import scraplyrics  # on importe la bibliothèque scraplyrics
+import json  # on importe le fichier json
+import boyermoore  # on importe le fichier boyer_moore
+import matplotlib.pyplot as plt  # on importe la bibliothèque matplotlib
 
 
-def openData(jsonFilePath):    
-    file = open(str(jsonFilePath), "r")    #on ouvre le fichier json
-    data = json.load(file)      #on lis le fichier json
+def openData(jsonFilePath):
+    file = open(str(jsonFilePath), "r")  # on ouvre le fichier json
+    data = json.load(file)  # on lis le fichier json
     return data
 
 
 def main(url=None, printout=True):
     counter = {}
     text = scraplyrics.scrap(url)
-    lexicalFieldData = openData("champLexicaux.json")     #on ouvre le fichier champLexicaux.json
-    #champLexicaux.json qui contient tous les champs lexicaux qu'on utilise
+    # on ouvre le fichier champLexicaux.json
+    lexicalFieldData = openData("champLexicaux.json")
+    # champLexicaux.json qui contient tous les champs lexicaux qu'on utilise
     totalWordsFound = 0
     for champ in lexicalFieldData.keys():
         counter[champ] = 0
@@ -23,15 +24,17 @@ def main(url=None, printout=True):
             counter[champ] += len(returnList)
             totalWordsFound += len(returnList)
     if printout == True:
-        altCounter = counter.copy()  
+        altCounter = counter.copy()
         print("Voici les thèmes trouvés:")
         for champ in counter.keys():
             if counter[champ] != 0:
                 print(str(champ)+" : " +
-                    str(round(counter[champ]/totalWordsFound*100))+" %")
+                      str(round(counter[champ]/totalWordsFound*100))+" %")
             else:
-                del altCounter[champ]     #on supprime ce qu'il y a dans altCounter
-        plt.barh(range(len(altCounter)), list(altCounter.values()), align='center')
+                # on supprime ce qu'il y a dans altCounter
+                del altCounter[champ]
+        plt.barh(range(len(altCounter)), list(
+            altCounter.values()), align='center')
         plt.yticks(range(len(altCounter)), list(altCounter.keys()))
         plt.show()
     return altCounter
@@ -45,38 +48,44 @@ if __name__ == '__main__':
     from kivy.uix.boxlayout import BoxLayout
     from kivy.core.window import Window
 
-    class UIApp(App):   #Classe qui régit l'application
-        def build(self):    #Méthode de construction de la fenêtre
-            self.title = 'Analyzer' #titre de la fenêtre
-            Window.clearcolor = (0.3, 0.3, 0.3, 1)  #couleur de fond
-            b = BoxLayout(orientation='vertical', padding=10)   #layout haut en bas 
+    class UIApp(App):  # Classe qui régit l'application
+        def build(self):  # Méthode de construction de la fenêtre
+            self.title = 'Analyzer'  # titre de la fenêtre
+            Window.clearcolor = (0.3, 0.3, 0.3, 1)  # couleur de fond
+            # layout haut en bas
+            b = BoxLayout(orientation='vertical', padding=10)
             l = Label(text="Lyrics analyser",
-                      font_size=50, bold=True)  #titre
-            b1 = BoxLayout(padding=40)  #box horizontale pour l'input et son label
-    
-            l1 = Label(text="URL de la page Genius : ", font_size=20)   #Label pour l'input
+                      font_size=50, bold=True)  # titre
+            # box horizontale pour l'input et son label
+            b1 = BoxLayout(padding=40)
+
+            l1 = Label(text="URL de la page Genius : ",
+                       font_size=20)  # Label pour l'input
             i1 = TextInput(font_size=20, height=50, padding=10, background_color=(
-                0.2, 0.2, 0.2, 1), foreground_color=(1, 1, 1, 1)) #l'input en question pour l'URL
-            b1.add_widget(l1)   #on ajoute à la box horizontale
-            b1.add_widget(i1)   #idem
+                0.2, 0.2, 0.2, 1), foreground_color=(1, 1, 1, 1))  # l'input en question pour l'URL
+            b1.add_widget(l1)  # on ajoute à la box horizontale
+            b1.add_widget(i1)  # idem
 
-            b.add_widget(l) #on ajoute le titre à la main box
-            b.add_widget(b1)    #on ajoute la box horizontale à la main box
+            b.add_widget(l)  # on ajoute le titre à la main box
+            b.add_widget(b1)  # on ajoute la box horizontale à la main box
 
-            okButton = Button(text="Start") #bouton pour lancer (on lui met la fonction plus tard)
+            # bouton pour lancer (on lui met la fonction plus tard)
+            okButton = Button(text="Start")
             b.add_widget(okButton)
 
             returnZone = TextInput(text="", font_size=20, padding=20, background_color=(
                 0.2, 0.2, 0.2, 1), foreground_color=(1, 1, 1, 1))   # zone dans laquelle on va mettre le return de main()
             b.add_widget(returnZone)
 
-            def startMainProgram(instance): #on doit définir la fonction dans le build() sinon ça ne marche pas
-                url = i1.text   #on récupère l'URL entrée
+            # on doit définir la fonction dans le build() sinon ça ne marche pas
+            def startMainProgram(instance):
+                url = i1.text  # on récupère l'URL entrée
                 if url == "" or "/" not in url:
                     returnZone.text = "URL Invalide"
-                else:    
+                else:
                     returnZone.text = str(main(url, True))
-            okButton.bind(on_release=startMainProgram)  #on ajoute la fonction au bouton
-            return b    #on return la box principale comme racine du programme
-            
+            # on ajoute la fonction au bouton
+            okButton.bind(on_release=startMainProgram)
+            return b  # on return la box principale comme racine du programme
+
     UIApp().run()
--- scraplyrics.py
+++ scraplyrics.py
@@ -1,23 +1,25 @@
 def scrap(site):
-    import bs4 as bs
+    import bs4 as bs  # library de scrapping
     import urllib.request
 
-    siteSplitted = site.split("/")
-    lyricsName=str(siteSplitted[len(siteSplitted)-1])
-    vraiSite= "https://genius.com/amp/"+lyricsName
-    print(lyricsName)
-    print(vraiSite)
+    siteSplitted = site.split("/")  # on sépare le tring
+    # on récupère le dernier de la liste correspondant à par exemple: Alpha-wann-soldat-tue-soldat-lyrics
+    lyricsName = str(siteSplitted[len(siteSplitted)-1])
+    vraiSite = "https://genius.com/amp/"+lyricsName  # on recrée la bonne url
+    # pour éviter le rejet on se fait passer pour un navigateur lambda
     hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib.request.Request(vraiSite,headers=hdr)
-    saucel=urllib.request.urlopen(req)
-    soupl = bs.BeautifulSoup(saucel, 'html')
+    # on prépare une requete http
+    req = urllib.request.Request(vraiSite, headers=hdr)
+    saucel = urllib.request.urlopen(req)  # on la fait
+    soupl = bs.BeautifulSoup(saucel, 'html')    # on scrappe!
 
-
-    tmp = soupl.find('div', "lyrics")
+    tmp = soupl.find('div', "lyrics")  # on récupère le div lyrics
+    # on écrit dans le fichier les paroles
     lyrics = open('temp/lyrics.txt', 'w+', encoding='utf-8')
     lyrics.write(tmp.text)
     lyrics.close()
     return tmp.text
+
 
 if __name__ == '__main__':
     scrap("https://genius.com/Alpha-wann-soldat-tue-soldat-lyrics")
